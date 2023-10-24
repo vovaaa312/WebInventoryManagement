ca1fa8d50fb8ec33ba26555d356b54fe57911388
Makarenko Vladimir <makarenkovladimir57@gmail.com>
2023-10-24 16:16:06 +0200

    Add project files.

---
+++ Models/Data/Category.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace WebInventoryManagement.Models.Data
+{
+    public enum Category
+    {
+        Meat, Fish, Dairy, Vegetables, Fruits, Freezer, Alcohol, DriedGoods, Snacks, Care, None
+    }
+
+    public static class CategoryInfo
+    {
+        public const int Count = 11;
+
+        public static IEnumerable Items
+        {
+            get
+            {
+                List<Category> clubList = new();
+                clubList.Add(Category.None);
+                clubList.Add(Category.Meat);
+                clubList.Add(Category.Fish);
+                clubList.Add(Category.Dairy);
+                clubList.Add(Category.Vegetables);
+                clubList.Add(Category.Fruits);
+                clubList.Add(Category.Freezer);
+                clubList.Add(Category.Alcohol);
+                clubList.Add(Category.DriedGoods);
+                clubList.Add(Category.Snacks);
+                clubList.Add(Category.Care);
+
+                return clubList;
+
+            }
+        }
+    }
+}
---
+++ Models/Data/Shelf.cs
@@ -0,0 +1,10 @@
+﻿namespace WebInventoryManagement.Models.Data
+{
+    public class Shelf
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int ItemsCount { get; set; } = 0;
+        public int Capacity { get; set; }
+    }
+}
